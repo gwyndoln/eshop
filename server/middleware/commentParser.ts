4965622eb27ae2023e1c185364a8a285d39bfadb
leo <lolkaazhaha@gmail.com>
2024-02-21 17:20:55 +0400

    Refactor the uploadDir value

--- server/middleware/commentParser.ts
+++ server/middleware/commentParser.ts
@@ -1,73 +1,73 @@
 import { Request, Response, NextFunction } from 'express';
 import formidable from 'formidable';
-import path from 'path';
 import ApiError from '../error/api-error';
 import checkFileType from '../functions/checkFileType';
+import { commentsUploadDir as uploadDir } from '../variables/variables';
 
 const commentParser = (req: Request, res: Response, next: NextFunction) => {
-	const form = formidable({
-		allowEmptyFiles: false,
-		uploadDir: path.resolve(__dirname, '..', 'files'),
-		multiples: true,
-		maxFileSize: 50 * 1024 * 1024, //50mb
-		minFileSize: 50 * 1024, // 50kb
-		keepExtensions: true,
-	});
+  const form = formidable({
+    allowEmptyFiles: false,
+    uploadDir,
+    multiples: true,
+    maxFileSize: 50 * 1024 * 1024, //50mb
+    minFileSize: 50 * 1024, // 50kb
+    keepExtensions: true,
+  });
 
-	let filesCounter = 0;
+  let filesCounter = 0;
 
-	form.onPart = (part) => {
-		const allowedImageTypes = ['image/jpeg', 'image/png'];
-		const allowedVideoTypes = ['video/mp4', 'video/webm'];
-		const imageRegExp = /\.(jpeg|jpg|png)$/;
-		const VideoRegExp = /\.(mp4|webm)$/;
+  form.onPart = (part) => {
+    const allowedImageTypes = ['image/jpeg', 'image/png'];
+    const allowedVideoTypes = ['video/mp4', 'video/webm'];
+    const imageRegExp = /\.(jpeg|jpg|png)$/;
+    const VideoRegExp = /\.(mp4|webm)$/;
 
-		const field = part.name;
-		let checkRegExp;
-		let allowedFileTypes;
+    const field = part.name;
+    let checkRegExp;
+    let allowedFileTypes;
 
-		if (field === 'text' || field === 'userId') {
-			return form._handlePart(part);
-		}
+    if (field === 'text' || field === 'userId') {
+      return form._handlePart(part);
+    }
 
-		if (field === 'image') {
-			checkRegExp = imageRegExp;
-			allowedFileTypes = allowedImageTypes;
-		}
+    if (field === 'image') {
+      checkRegExp = imageRegExp;
+      allowedFileTypes = allowedImageTypes;
+    }
 
-		if (field === 'video') {
-			checkRegExp = VideoRegExp;
-			allowedFileTypes = allowedVideoTypes;
-		}
+    if (field === 'video') {
+      checkRegExp = VideoRegExp;
+      allowedFileTypes = allowedVideoTypes;
+    }
 
-		const isFileCorrect = checkFileType(
-			part,
-			checkRegExp as RegExp,
-			allowedFileTypes as string[]
-		);
+    const isFileCorrect = checkFileType(
+      part,
+      checkRegExp as RegExp,
+      allowedFileTypes as string[]
+    );
 
-		if (!isFileCorrect) {
-			return next(ApiError.UnsuportedMedia('Неправильный тип данных'));
-		}
+    if (!isFileCorrect) {
+      return next(ApiError.UnsuportedMedia('Неправильный тип данных'));
+    }
 
-		filesCounter++;
+    filesCounter++;
 
-		if (filesCounter > 4) {
-			return next(ApiError.BadRequest('Слишком много файлов'));
-		}
+    if (filesCounter > 4) {
+      return next(ApiError.BadRequest('Слишком много файлов'));
+    }
 
-		form._handlePart(part);
-	};
+    form._handlePart(part);
+  };
 
-	form.parse(req, (err, fields, files) => {
-		if (err) {
-			next(err);
-		}
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+    }
 
-		req.body = { ...fields, ...files };
+    req.body = { ...fields, ...files };
 
-		next();
-	});
+    next();
+  });
 };
 
-export default commentParser;
+export { uploadDir, commentParser };

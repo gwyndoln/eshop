57f8850b927a49ea171765d8d4eced027a380535
leo <lolkaazhaha@gmail.com>
2024-02-21 19:58:46 +0400

    Config prettier, eslint and typescript

---
+++ client/eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	// env: {
+	// 	browser: true,
+	// 	es2021: true,
+	// },
+	// extends: [
+	// 	'plugin:react/recommended',
+	// 	'airbnb',
+	// 	'airbnb-typescript',
+	// 	'prettier',
+	// ],
+	// overrides: [
+	// 	{
+	// 		files: ['*.ts', '*.tsx'],
+	// 		parserOptions: {
+	// 			project: ['./tsconfig.json'],
+	// 		},
+	// 	},
+	// ],
+	// parser: '@typescript-eslint/parser',
+	// parserOptions: {
+	// 	ecmaFeatures: {
+	// 		jsx: true,
+	// 	},
+	// 	ecmaVersion: 'latest',
+	// 	tsconfigRootDir: __dirname,
+	// 	sourceType: 'module',
+	// },
+	// plugins: ['react', '@typescript-eslint'],
+	// rules: {
+	// 	'react/react-in-jsx-scope': 'off',
+	// 	'no-console': 'off',
+	// 	'react/function-component-definition': 'off',
+	// 	'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+	// },
+};

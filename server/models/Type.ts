d6cc8fb084ba5c0fc9ba92dd93b4316385bc80c1
leo <lolkaazhaha@gmail.com>
2024-02-21 16:52:43 +0400

    Add url, image and uuid columns to the type model

--- server/models/Type.ts
+++ server/models/Type.ts
@@ -1,3 +1,4 @@
+import CyrillicToTranslit from 'cyrillic-to-translit-js';
 import {
 	BelongsTo,
 	Column,
@@ -9,10 +10,39 @@ import {
 } from 'sequelize-typescript';
 import { Product } from './Product';
 
+const cyrillicToTranslit = new (CyrillicToTranslit as any)();
+
+
 @Table
 export class Type extends Model {
+	@Column({defaultValue: DataType.UUIDV4, type: DataType.UUID})
+	uuid!: string;
+
 	@Column({ type: DataType.STRING(50), allowNull: false })
 	title!: string;
+
+	@Column({
+		type: DataType.STRING(300),
+		allowNull: false,
+		set(value: string[]) {
+			this.setDataValue('image', value);
+		},
+		get() {
+			return this.getDataValue('image');
+		},
+	})
+	image!: string;
+
+	@Column({
+		type: DataType.VIRTUAL,
+		set() {
+			throw new Error('Нельзя задать `url` значение')
+		},
+		get() {
+			return cyrillicToTranslit.transform((this as Type).title, '-').toLowerCase()
+		}
+	})
+	url!: string;
 
 	@ForeignKey(() => Type)
 	@Column

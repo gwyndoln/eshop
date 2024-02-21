5dfffd54600f877810f9e35c8076a2c18ec17fa3
leo <lolkaazhaha@gmail.com>
2024-02-21 15:40:25 +0400

    Add a lower case transformation to brand and type validation chains

--- server/validation/productValidator.ts
+++ server/validation/productValidator.ts
@@ -6,8 +6,8 @@ export const productValidatior = [
 	body('title').trim().escape(),
 	body('price').trim().escape(),
 	body('description').trim().escape(),
-	body('brand').trim().escape(),
-	body('type').trim().escape(),
+	body('brand').trim().toLowerCase().escape(),
+	body('type').trim().toLowerCase().escape(),
 	(req: Request, res: Response, next: NextFunction) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {

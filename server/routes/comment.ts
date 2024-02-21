575120cc85d395c341058b5761f5badd1d221432
leo <lolkaazhaha@gmail.com>
2024-02-21 16:47:56 +0400

    Add the delete comment route

--- server/routes/comment.ts
+++ server/routes/comment.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import {
 	createComment,
+	deleteComment,
 	editComment,
 	getComments,
 } from '../controllers/comment';
-import commentParser from '../middleware/commentParser';
+import {commentParser} from '../middleware/commentParser';
 import editCommentParser from '../middleware/editCommentParser';
 import { commentValidatior } from '../validation/commentValidator';
 
@@ -13,6 +14,7 @@ const router = Router();
 router
 	.post('/:productCode', commentParser, commentValidatior, createComment)
 	.get('/:productCode', getComments)
-	.patch('/', editCommentParser, commentValidatior, editComment);
+	.patch('/', editCommentParser, commentValidatior, editComment)
+	.delete('/', deleteComment)
 
 export default router;

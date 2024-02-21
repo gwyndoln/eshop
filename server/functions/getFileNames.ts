78bb004dde25d363422b8b31294cda397f75eaa9
leo <lolkaazhaha@gmail.com>
2024-02-21 15:11:50 +0400

    Add a missing file error condition

--- server/functions/getFileNames.ts
+++ server/functions/getFileNames.ts
@@ -1,11 +1,16 @@
 import formidable from 'formidable';
+import ApiError from '../error/api-error';
 
 const getFileNames = (file: formidable.File | formidable.File[]) => {
-	if (Array.isArray(file)) {
-		return file.map((item) => item.newFilename);
-	}
+  if (!file) {
+    throw ApiError.BadRequest('Нет файлов');
+  }
 
-	return [file.newFilename];
+  if (Array.isArray(file)) {
+    return file.map((item) => item.newFilename);
+  }
+
+  return [file.newFilename];
 };
 
 export default getFileNames;

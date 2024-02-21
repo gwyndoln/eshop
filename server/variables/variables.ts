c4741e8d66d620c6d900a8e3d1547c74defa7f6a
leo <lolkaazhaha@gmail.com>
2024-02-21 15:27:04 +0400

    Add variables to file paths on the server

---
+++ server/variables/variables.ts
@@ -0,0 +1,7 @@
+import path from 'path';
+
+const typeUploadDir = path.resolve(__dirname, '..', 'files', 'type');
+const commentsUploadDir = path.resolve(__dirname, '..', 'files', 'comment');
+const productUploadDir = path.resolve(__dirname, '..', 'files', 'product');
+
+export { typeUploadDir, commentsUploadDir, productUploadDir };
